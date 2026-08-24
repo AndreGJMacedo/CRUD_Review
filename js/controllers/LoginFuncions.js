8d6e6dca8ae73ac290b179f336637ea850e2eafd
André Gomes <andregjm95@gmail.com>
2026-08-24 14:55:22 -0300

    feat: Ajuste na ativação para o campo senha e inicio da tela de registro

--- js/controllers/LoginFuncions.js
+++ js/controllers/LoginFuncions.js
@@ -1,5 +1,7 @@
+let pw = document.querySelector('#psw');
+let bt = document.querySelector('#lg_btn');
+
 function showPass() {
-    let pw = document.querySelector('#psw');
     if (pw.type === 'password') {
         pw.type = 'text';
     }
@@ -9,10 +11,7 @@ function showPass() {
 };
 
 function validate_user() {
-    let usr = document.querySelector('#user_nm');
-    let bt = document.querySelector('#lg_btn');
-
-    if (usr.value !== '') {
+    if (pw.value !== '') {
         bt.disabled = false;
     }else {
         bt.disabled = true;
